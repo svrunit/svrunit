f24c9a9d9251592fca99c41f7c8ba5d7c96bcbef
Christian Dangl <boxblinkracer@gmx.net>
2023-02-11 14:50:55 +0100

    fix broken yaml parsing

--- src/Services/TestParser/TestSpecFileParser.php
+++ src/Services/TestParser/TestSpecFileParser.php
@@ -11,7 +11,7 @@ use SVRUnit\Components\Tests\Adapters\FileExistsTest;
 use SVRUnit\Components\Tests\Adapters\FilePermissionTest;
 use SVRUnit\Components\Tests\Adapters\PhpIniTest;
 use SVRUnit\Components\Tests\Adapters\PhpModuleTest;
-
+use Symfony\Component\Yaml\Parser;
 
 class TestSpecFileParser
 {
@@ -36,7 +36,8 @@ class TestSpecFileParser
             throw new FileNotFoundException('Test File not found: ' . $testsFile);
         }
 
-        $parsed = yaml_parse_file($testsFile);
+        $parser = new Parser();
+        $parsed = $parser->parse((string)file_get_contents($testsFile));
 
         if ($parsed === null) {
             throw new \Exception("Error when reading tests from file: " . $testsFile);
